ca4c83dc854a593b6b3ea22a094fb99109152ea9
Jander da Costa Nery <jander.webmaster@gmail.com>
2025-10-02 10:03:44 -0400

    Feat: backend prisma

--- backend-quiz/src/main.ts
+++ backend-quiz/src/main.ts
@@ -5,6 +5,9 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
   const config = app.get(ConfigService);
   const logger = new Logger();
   const PORT = config.get<number>('SERVER_PORT_QUIZ') || 3333;
